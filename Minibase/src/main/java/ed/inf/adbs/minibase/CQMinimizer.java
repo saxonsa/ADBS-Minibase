b25ab0ee2e1730d6c444ea8416a99c7ec1d72415
Xingyun SA <saxonsa5683@163.com>
2023-02-08 03:36:14 +0000

    add partial homo

--- Minibase/src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
+++ Minibase/src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
@@ -8,9 +8,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  *
@@ -48,25 +48,103 @@ public class CQMinimizer {
         // iterate every atom in body to choose one for minimization
         List<Atom> body = query.getBody();
 
-        for (int i = 0; i < body.size(); i++) {
+        for (Atom currentAtom : body) {
             // test if "body \ {atom}" contains x1...xk in head
             List<Atom> bodyCloned = new ArrayList<>(body);
-            Atom removedAtom = bodyCloned.remove(i);
-            if(!checkFreeVariablesContained(bodyCloned, head)) {
+            bodyCloned.remove(currentAtom);
+            if (!checkFreeVariablesContained(bodyCloned, head)) {
                 continue;
             }
 
             // test if there is a query homomorphism from "Q(x1...xk) :- body" to "Q(x1...xk) :- body\{atom}"
-
+            if (!checkHomomorphismExistence(body, bodyCloned)) {
+                // Not exist
+                continue;
+            }
 
             // if true, update body to body \ {atom}
-
-            // else continue
-
-            // until the last atom
+            body.remove(currentAtom);
         }
 
         outputMinimizedQuery(query, outputFile);
+    }
+
+    /**
+     * Check if there is a query homomorphism from the original query to the minimized query
+     */
+    public static boolean checkHomomorphismExistence(List<Atom>originalBody, List<Atom>minimizedBody) {
+        // extract relation name from both body query and check if they match
+        HashSet<String> originalRelation = new HashSet<>();
+        HashSet<String> minimizedRelation = new HashSet<>();
+        for (Atom a : originalBody) {
+            RelationalAtom atom = (RelationalAtom) a;
+            originalRelation.add(atom.getName());
+        }
+        for (Atom a : minimizedBody) {
+            RelationalAtom atom = (RelationalAtom) a;
+            minimizedRelation.add(atom.getName());
+        }
+        if (!minimizedRelation.containsAll(originalRelation)) {
+            System.out.println("relation of original body: " + originalRelation);
+            System.out.println("relation of minimized body: " + minimizedRelation);
+            return false;
+        }
+
+        // for each Relation (R, T, S...)
+        ArrayList<Object> homo = new ArrayList<>();
+        for (String r : originalRelation) {
+
+            // find all relations associated with table r in both original and minimized body
+            List<RelationalAtom> originalBodyWithRelationR = new ArrayList<>();
+            List<RelationalAtom> minimizedBodyWithRelationR = new ArrayList<>();
+            for (Atom a : originalBody) {
+                RelationalAtom atom = (RelationalAtom) a;
+                if (atom.getName().equals(r)) {
+                    originalBodyWithRelationR.add(atom);
+                }
+            }
+            for (Atom a : minimizedBody) {
+                RelationalAtom atom = (RelationalAtom) a;
+                if (atom.getName().equals(r)) {
+                    minimizedBodyWithRelationR.add(atom);
+                }
+            }
+
+            // List all the mappings from original query to minimized query
+
+            ArrayList<Object> homoOfEachRelation = new ArrayList<>();
+            for (RelationalAtom originalRA : originalBodyWithRelationR) {
+
+                // homo of each sub relation: like [(t1->t2, t3->t4), (s1->s2, s3->s3)]
+                ArrayList<Object> homoOfEachSubRelation = new ArrayList<>();
+
+                for (RelationalAtom minimizedRA : minimizedBodyWithRelationR) {
+                    // unit homomorphism, Like (t1->t2, t3->t4)
+                    HashMap<Term, Term> unitHomo = new HashMap<>();
+                    List<Term> originalBodyTerms = originalRA.getTerms();
+                    List<Term> minimizedBodyTerms = minimizedRA.getTerms();
+                    for (int i = 0; i < originalBodyTerms.size(); i++) {
+                        Term originalBodyTerm = originalBodyTerms.get(i);
+                        Term minimizedBodyTerm = minimizedBodyTerms.get(i);
+                        // check if the unit homomorphism is valid
+                        // avoid constant -> variable
+                        if (!((originalBodyTerm instanceof Constant) && (minimizedBodyTerm instanceof Variable))) {
+                            unitHomo.put(originalBodyTerm, minimizedBodyTerm);
+                        }
+                    }
+                    homoOfEachSubRelation.add(unitHomo);
+                }
+                homoOfEachRelation.add(homoOfEachSubRelation);
+            }
+            homo.add(homoOfEachRelation);
+        }
+
+        // select correct homomorphism combination
+
+
+
+
+        return true;
     }
 
     /**
