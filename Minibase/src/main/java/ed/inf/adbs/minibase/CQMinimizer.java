fee108ab0cbb6a3a268525904b4f6c3656cb9028
Xingyun SA <saxonsa5683@163.com>
2023-02-11 01:24:38 +0000

    add homomorphism mechanism, task1 basically finished

--- Minibase/src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
+++ Minibase/src/main/java/ed/inf/adbs/minibase/CQMinimizer.java
@@ -7,10 +7,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -48,7 +45,8 @@ public class CQMinimizer {
         // iterate every atom in body to choose one for minimization
         List<Atom> body = query.getBody();
 
-        for (Atom currentAtom : body) {
+        for (Iterator<Atom> iterator = body.iterator(); iterator.hasNext();) {
+            Atom currentAtom = iterator.next();
             // test if "body \ {atom}" contains x1...xk in head
             List<Atom> bodyCloned = new ArrayList<>(body);
             bodyCloned.remove(currentAtom);
@@ -63,7 +61,7 @@ public class CQMinimizer {
             }
 
             // if true, update body to body \ {atom}
-            body.remove(currentAtom);
+            iterator.remove();
         }
 
         outputMinimizedQuery(query, outputFile);
@@ -73,7 +71,7 @@ public class CQMinimizer {
      * Check if there is a query homomorphism from the original query to the minimized query
      */
     public static boolean checkHomomorphismExistence(List<Atom>originalBody, List<Atom>minimizedBody) {
-        // extract relation name from both body query and check if they match
+        // extract relation name from both body query and check if they match, Like {R, S, T...}
         HashSet<String> originalRelation = new HashSet<>();
         HashSet<String> minimizedRelation = new HashSet<>();
         for (Atom a : originalBody) {
@@ -87,12 +85,14 @@ public class CQMinimizer {
         if (!minimizedRelation.containsAll(originalRelation)) {
             System.out.println("relation of original body: " + originalRelation);
             System.out.println("relation of minimized body: " + minimizedRelation);
+            System.out.println("cannot remove this atom due to the relation table does not match!");
             return false;
         }
 
         // for each Relation (R, T, S...)
-        ArrayList<Object> homo = new ArrayList<>();
+        ArrayList<ArrayList<ArrayList<HashMap<String, String>>>> homo = new ArrayList<>();
         for (String r : originalRelation) {
+            boolean validHomo = true;
 
             // find all relations associated with table r in both original and minimized body
             List<RelationalAtom> originalBodyWithRelationR = new ArrayList<>();
@@ -112,38 +112,132 @@ public class CQMinimizer {
 
             // List all the mappings from original query to minimized query
 
-            ArrayList<Object> homoOfEachRelation = new ArrayList<>();
+            ArrayList<ArrayList<HashMap<String, String>>> homoOfEachRelation = new ArrayList<>();
             for (RelationalAtom originalRA : originalBodyWithRelationR) {
 
                 // homo of each sub relation: like [(t1->t2, t3->t4), (s1->s2, s3->s3)]
-                ArrayList<Object> homoOfEachSubRelation = new ArrayList<>();
+                ArrayList<HashMap<String, String>> homoOfEachSubRelation = new ArrayList<>();
 
                 for (RelationalAtom minimizedRA : minimizedBodyWithRelationR) {
+                    boolean validUnitHomo = true;
                     // unit homomorphism, Like (t1->t2, t3->t4)
-                    HashMap<Term, Term> unitHomo = new HashMap<>();
+                    HashMap<String, String> unitHomo = new HashMap<>();
                     List<Term> originalBodyTerms = originalRA.getTerms();
                     List<Term> minimizedBodyTerms = minimizedRA.getTerms();
                     for (int i = 0; i < originalBodyTerms.size(); i++) {
                         Term originalBodyTerm = originalBodyTerms.get(i);
                         Term minimizedBodyTerm = minimizedBodyTerms.get(i);
                         // check if the unit homomorphism is valid
-                        // avoid constant -> variable
-                        if (!((originalBodyTerm instanceof Constant) && (minimizedBodyTerm instanceof Variable))) {
-                            unitHomo.put(originalBodyTerm, minimizedBodyTerm);
+                        if (checkValidUnitHomoMapping(originalBodyTerm, minimizedBodyTerm)) {
+                            unitHomo.put(originalBodyTerm.toString(), minimizedBodyTerm.toString());
+                        } else {
+                            validUnitHomo = false;
+                            break;
                         }
                     }
-                    homoOfEachSubRelation.add(unitHomo);
+                    if (validUnitHomo) {
+                        homoOfEachSubRelation.add(unitHomo);
+                    }
+
                 }
-                homoOfEachRelation.add(homoOfEachSubRelation);
+
+                if (homoOfEachSubRelation.size() > 0) {
+                    homoOfEachRelation.add(homoOfEachSubRelation);
+                } else {
+                    validHomo = false;
+                    break;
+                }
             }
-            homo.add(homoOfEachRelation);
+
+            if (validHomo) {
+                homo.add(homoOfEachRelation);
+            } else {
+                break;
+            }
         }
 
-        // select correct homomorphism combination
+
+        System.out.println("Homo: " + homo);
+
+        if (homo.size() == 0) {
+            return false;
+        }
 
 
+        // A dfs algorithm trying to explore all the possible mappings
 
+        ArrayList<ArrayList<HashMap<String, String>>> groups = new ArrayList<>();
+        for (ArrayList<ArrayList<HashMap<String, String>>> homoOfEachRelation : homo) {
+            groups.addAll(homoOfEachRelation);
+        }
 
+        ArrayList<HashMap<String, String>> validHomoCombinations = getValidHomoCombinations(groups);
+
+        System.out.println("query homomorphism: " + validHomoCombinations);
+
+        return validHomoCombinations.size() > 0;
+
+//        return false;
+    }
+
+    public static ArrayList<HashMap<String, String>> getValidHomoCombinations(ArrayList<ArrayList<HashMap<String, String>>> groups) {
+        ArrayList<HashMap<String, String>> validHomoCombinations = new ArrayList<>();
+        dfs(groups, 0, new HashMap<>(), validHomoCombinations);
+        return validHomoCombinations;
+    }
+
+    public static void dfs(ArrayList<ArrayList<HashMap<String, String>>> groups,
+                      int groupIndex, HashMap<String, String> currentHomoCombination,
+                      ArrayList<HashMap<String, String>> validHomoCombinations) {
+        if (groupIndex == groups.size()) {
+            // All groups have been processed
+            validHomoCombinations.add(currentHomoCombination);
+            return;
+        }
+
+        // explore all the possible choices in the current group
+        ArrayList<HashMap<String, String>> group = groups.get(groupIndex);
+        for (HashMap<String, String> choice : group) {
+            HashMap<String, String> beforeNextHomo = new HashMap<>(currentHomoCombination);
+            if (!checkValidHomoCombination(currentHomoCombination, choice)) {
+                continue;
+            }
+            dfs(groups, groupIndex + 1, currentHomoCombination, validHomoCombinations);
+
+            // remove the original future choice
+            currentHomoCombination = new HashMap<>(beforeNextHomo);
+        }
+    }
+
+    public static boolean checkValidHomoCombination(HashMap<String, String> homoCombination, HashMap<String, String> choice) {
+        HashMap<String, String> beforeHomoCombination = new HashMap<>(homoCombination);
+        for (Map.Entry<String, String> entry : choice.entrySet()) {
+            if (!beforeHomoCombination.containsKey(entry.getKey())) {
+                beforeHomoCombination.put(entry.getKey(), entry.getValue());
+            } else {
+                if (!Objects.equals(beforeHomoCombination.get(entry.getKey()), entry.getValue())) {
+                    // back track it to the situation when the new choice has not been merged
+                    return false;
+                }
+            }
+        }
+
+        homoCombination = beforeHomoCombination;
+        return true;
+    }
+
+    /**
+    * check if a unit homomorphism mapping (x->y) is valid
+     * Specifically, avoid constant->variable and constant1->constant2(c1 \neq c2)
+     */
+    public static boolean checkValidUnitHomoMapping(Term originalBodyTerm, Term minimizedBodyTerm) {
+        if (originalBodyTerm instanceof Constant) {
+            return (minimizedBodyTerm instanceof Constant) &&
+                    (Objects.equals(originalBodyTerm.toString(), minimizedBodyTerm.toString()));
+        } else if (originalBodyTerm instanceof Variable) {
+            return true;
+        }
+        // we temporarily skip the check for aggregation term
         return true;
     }
 
@@ -200,7 +294,7 @@ public class CQMinimizer {
     public static void outputMinimizedQuery(Query query, String filename) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
-            writer.write(query.toString());
+            writer.write(query.toString() + '\n');
             writer.close();
         } catch (IOException e) {
             System.err.println("Exception occurred during writer minimized query to output/query{?}.txt");
