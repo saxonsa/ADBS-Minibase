aedf7cb0bed8b08925efd0ff657cddbe12213446
Xingyun(Saxon) SA <s2305139@sms.ed.ac.uk>
2023-03-05 21:58:37 +0000

    feat: test case of scan Opt

--- Minibase/src/main/java/ed/inf/adbs/minibase/operator/db/Schema.java
+++ Minibase/src/main/java/ed/inf/adbs/minibase/operator/db/Schema.java
@@ -7,6 +7,12 @@ import java.util.List;
 
 public class Schema {
     private final String relationName;
+
+    /**
+     * The type of attribute are stored in Class format
+     * int: ed.inf.adbs.minibase.base.IntegerConstant
+     * string: ed.inf.adbs.minibase.StringConstant
+     */
     private final List<Class<? extends Constant>> attributeTypes;
 
     public Schema(String relationName, List<Class<? extends Constant>> attributesTypes) {
