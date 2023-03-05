e698407ab0b49c902e791d0778b0bed6efc41944
Xingyun(Saxon) SA <s2305139@sms.ed.ac.uk>
2023-03-05 22:55:13 +0000

    add: QueryPlan to support ScanOperator

--- Minibase/src/main/java/ed/inf/adbs/minibase/Minibase.java
+++ Minibase/src/main/java/ed/inf/adbs/minibase/Minibase.java
@@ -1,5 +1,14 @@
 package ed.inf.adbs.minibase;
 
+import ed.inf.adbs.minibase.base.Query;
+import ed.inf.adbs.minibase.operator.common.QueryPlan;
+import ed.inf.adbs.minibase.operator.common.ResultWriter;
+import ed.inf.adbs.minibase.operator.db.Catalog;
+import ed.inf.adbs.minibase.parser.QueryParser;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 /**
  * In-memory database system
  *
@@ -17,11 +26,19 @@ public class Minibase {
         String inputFile = args[1];
         String outputFile = args[2];
 
-//        evaluateCQ(databaseDir, inputFile, outputFile);
+        evaluateCQ(databaseDir, inputFile, outputFile);
     }
 
     public static void evaluateCQ(String databaseDir, String inputFile, String outputFile) {
-        // TODO: add your implementation
-
+        try {
+            Catalog catalog = Catalog.getCatalog();
+            catalog.init(databaseDir);
+            Query query = QueryParser.parse(Paths.get(inputFile));
+            QueryPlan plan = new QueryPlan(query);
+            ResultWriter.init(outputFile);
+            plan.getRoot().dump();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
