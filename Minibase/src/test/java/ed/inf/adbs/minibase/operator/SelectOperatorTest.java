fad71ca51c0846405df0a8d71338122139f1c043
Xingyun(Saxon) SA <s2305139@sms.ed.ac.uk>
2023-03-07 22:48:45 +0000

    add: SelectOperator test - finish select

---
+++ Minibase/src/test/java/ed/inf/adbs/minibase/operator/SelectOperatorTest.java
@@ -0,0 +1,34 @@
+package ed.inf.adbs.minibase.operator;
+
+import ed.inf.adbs.minibase.operator.common.Interpreter;
+import ed.inf.adbs.minibase.operator.db.Catalog;
+import ed.inf.adbs.minibase.utils.CSVComparator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class SelectOperatorTest {
+    private Catalog catalog = null;
+
+    @Before
+    public void setup() {
+        catalog = Catalog.getCatalog();
+    }
+
+    @Test
+    public void testSelectTask1() throws IOException {
+        Interpreter interpreter = new Interpreter("data/evaluation/input/select_q1.txt", "data/evaluation/output/select_q1.csv");
+        interpreter.dump();
+        assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/select_q1.csv", "data/evaluation/expected_output/select_q1.csv"));
+    }
+
+    @Test
+    public void testSelectTask2() throws IOException {
+        Interpreter interpreter = new Interpreter("data/evaluation/input/select_q2.txt", "data/evaluation/output/select_q2.csv");
+        interpreter.dump();
+        assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/select_q2.csv", "data/evaluation/expected_output/select_q2.csv"));
+    }
+}
