583b5180d00a7a1240e4bf0e8027af4fa492d9dd
Xingyun(Saxon) SA <s2305139@sms.ed.ac.uk>
2023-03-08 15:24:06 +0000

    add: ProjectOperator

--- Minibase/src/main/java/ed/inf/adbs/minibase/operator/common/QueryPlan.java
+++ Minibase/src/main/java/ed/inf/adbs/minibase/operator/common/QueryPlan.java
@@ -1,12 +1,13 @@
 package ed.inf.adbs.minibase.operator.common;
 
-import ed.inf.adbs.minibase.base.ComparisonAtom;
-import ed.inf.adbs.minibase.base.Query;
-import ed.inf.adbs.minibase.base.RelationalAtom;
+import ed.inf.adbs.minibase.base.*;
 import ed.inf.adbs.minibase.operator.Operator;
+import ed.inf.adbs.minibase.operator.ProjectOperator;
 import ed.inf.adbs.minibase.operator.ScanOperator;
 import ed.inf.adbs.minibase.operator.SelectOperator;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +21,42 @@ public class QueryPlan {
     }
 
     private void constructQueryTree() {
+        // create a ScamOperator as a root for any queries
         List<RelationalAtom> relationalAtoms = query.getBody().stream()
                 .filter(RelationalAtom.class::isInstance)
                 .map(RelationalAtom.class::cast).collect(Collectors.toList());
         root = new ScanOperator(relationalAtoms.get(0));
 
-        // get all the predicates
+        // check if there is any selection predicates, if it is, create a root for SelectOperator
         List<ComparisonAtom> comparisonAtoms = query.getBody().stream()
                         .filter(ComparisonAtom.class::isInstance)
                                 .map(ComparisonAtom.class::cast).collect(Collectors.toList());
         if (comparisonAtoms.size() > 0) {
             root = new SelectOperator(root, relationalAtoms.get(0), comparisonAtoms);
         }
+
+        // check if the order of head has been changed, or if any terms in the head have been projected away
+        // if so, create a ProjectOperator as a root
+        Head head = query.getHead();
+        if ((head.getVariables().size() < relationalAtoms.get(0).getTerms().size()) ||
+                checkQueryHeadOrderChanged(head.getVariables(), relationalAtoms.get(0))) {
+            // some variables have been projected away
+            root = new ProjectOperator(root, query, relationalAtoms.get(0));
+        }
+    }
+
+    private boolean checkQueryHeadOrderChanged(List<Variable> head, RelationalAtom relationalAtom) {
+        List<Variable> body = new ArrayList<>();
+        for (Term term : relationalAtom.getTerms()) {
+            body.add((Variable) term);
+        }
+        
+        for (int i = 0; i < head.size(); i++) {
+            if (body.indexOf(head.get(i)) != i) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Operator getRoot() {
---
+++ Minibase/src/test/java/ed/inf/adbs/minibase/operator/ProjectOperatorTest.java
@@ -0,0 +1,34 @@
+package ed.inf.adbs.minibase.operator;
+
+import ed.inf.adbs.minibase.operator.common.Interpreter;
+import ed.inf.adbs.minibase.operator.db.Catalog;
+import ed.inf.adbs.minibase.utils.CSVComparator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class ProjectOperatorTest {
+    private final Catalog catalog = Catalog.getCatalog();
+
+    @Before
+    public void setupCatalog() {
+        catalog.init("data/evaluation/db");
+    }
+
+    @Test
+    public void testProjectTask1() throws IOException {
+        Interpreter interpreter = new Interpreter("data/evaluation/input/project_q1.txt", "data/evaluation/output/project_q1.csv");
+        interpreter.dump();
+        assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/project_q1.csv", "data/evaluation/expected_output/project_q1.csv"));
+    }
+
+    @Test
+    public void testProjectTask2() throws IOException {
+        Interpreter interpreter = new Interpreter("data/evaluation/input/project_q2.txt", "data/evaluation/output/project_q2.csv");
+        interpreter.dump();
+        assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/project_q2.csv", "data/evaluation/expected_output/project_q2.csv"));
+    }
+}
