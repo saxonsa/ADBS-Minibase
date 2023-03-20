a15a17484a277dd01890fabbecd698c9343c72bd
Xingyun SA <saxonsa5683@163.com>
2023-03-20 16:30:20 +0000

    add a test case for sum aggregation

--- Minibase/src/test/java/ed/inf/adbs/minibase/operator/SumOperatorTest.java
+++ Minibase/src/test/java/ed/inf/adbs/minibase/operator/SumOperatorTest.java
@@ -38,4 +38,11 @@ public class SumOperatorTest {
         interpreter.dump();
         assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/sum_q3.csv", "data/evaluation/expected_output/sum_q3.csv"));
     }
+
+    @Test
+    public void testSumOperatorQ4() throws IOException {
+        Interpreter interpreter = new Interpreter("data/evaluation/input/sum_q4.txt", "data/evaluation/output/sum_q4.csv");
+        interpreter.dump();
+        assertTrue(CSVComparator.areCSVFilesEqual("data/evaluation/output/sum_q4.csv", "data/evaluation/expected_output/sum_q4.csv"));
+    }
 }
